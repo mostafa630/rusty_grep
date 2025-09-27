948662343aec129effa8c8a9955c200bde882301
mostafa <mostafayounis630@gmail.com>
2025-09-27 14:58:17 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::env;
 use std::io;
 use std::process;
+use std::fs;
 
 mod Pattern;
 mod pattern_matcher;
 
 mod File;
 
+use anyhow::Error;
 use pattern_matcher::match_input;
 
 use crate::File::_File;
@@ -16,7 +18,7 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 || args[1] != "-E" {
+    if args.len() < 3 {
         println!("the input is not correct");
         process::exit(1);
     }
@@ -24,6 +26,19 @@ fn main() {
     let pattern = args[2].clone();
 
     if args.len() >= 4 {
+        if args[1] == "-r" {   // process folder (recursively search in that folder)
+            let folder_name = args[4].clone();
+            let pattern = args[3].clone();
+            let mut all_files = vec![];
+            match recursive_search(folder_name, &mut all_files) {
+                Ok(_) => process_files(all_files, pattern),
+                Err(e) => {
+                    eprintln!("Error reading folder: {}", e);
+                    process::exit(1);
+                }
+            }
+            
+        }
         let files = get_files_names_from_args(args);
         process_files(files, pattern);
     } else {
@@ -72,6 +87,20 @@ fn get_files_names_from_args(args: Vec<String>) -> Vec<String> {
         files_names.push(file_name.clone());
     }
     files_names
+}
+fn recursive_search(folder_path : String , founded_files : &mut Vec<String>)->io::Result<&mut Vec<String>>{
+    for entry in fs::read_dir(folder_path)? {
+        let entry = entry?;
+        let path = entry.path();
+        
+        if path.is_dir(){
+            recursive_search(path.to_string_lossy().to_string() , founded_files);
+        }
+        else{
+            founded_files.push(path.to_string_lossy().to_string());
+        }
+    }
+    return Ok(founded_files);
 }
 
 fn process_input_from_stdin(input_line: String, pattern: String) {
